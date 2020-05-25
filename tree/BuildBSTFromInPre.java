3b5691e28451d5bf266768df39629ac13b96d71d
yashkothari92 <yashkothari2020@gmail.com>
2020-05-25 18:42:51 +0530

    correction: TreeNode replaced with BSTNode

--- tree/BuildBSTFromInPre.java
+++ tree/BuildBSTFromInPre.java
@@ -10,23 +10,23 @@ public class BuildBSTFromInPre {
 		int[] pre = { 1, 2 }; // {3,9,20,15,7};
 		int[] in = { 2, 1 }; // {9,3,15,20,7};
 
-		TreeNode root = obj.buildTree(pre, in);
+		BSTNode root = obj.buildTree(pre, in);
 		System.out.println(root);
 	}
 
-	public TreeNode buildTree(int[] preorder, int[] inorder) {
+	public BSTNode buildTree(int[] preorder, int[] inorder) {
 		int inStartIdx = 0;
 		int inEndIdx = inorder.length - 1;
 		return buildTreeSub(preorder, inorder, inStartIdx, inEndIdx);
 	}
 
-	public TreeNode buildTreeSub(int[] preorder, int[] inorder, int inStartIdx, int inEndIdx) {
+	public BSTNode buildTreeSub(int[] preorder, int[] inorder, int inStartIdx, int inEndIdx) {
 		// when nothing to check (or empty), return null
 		if (inStartIdx > inEndIdx) {
 			return null;
 		}
 
-		TreeNode node = new TreeNode(preorder[preIdx++]);
+		BSTNode node = new BSTNode(preorder[preIdx++]);
 
 		// when single element, return it
 		if (inStartIdx == inEndIdx) {
